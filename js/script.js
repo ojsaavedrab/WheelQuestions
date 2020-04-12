d3ece94643985a07c2e15882c2754a99ee092f0e
ojsaavedrab <oscars@lagash.com>
2020-04-11 23:54:05 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Immediately invoked function expression
 // to not pollute the global scope
 $(function () {
-  //var data = {"title":"Teju's Shadow","questions":[{"id":0,"question":"How do you describe a shadow?","options":[{"id":"A","option":"like a dark picture"},{"id":"B","option":"like a spy"},{"id":"C","option":"like a monster"}],"value":100,"idAnswer":"B"},{"id":1,"question":"¿What is the child in the story afraid?","options":[{"id":"A","option":"of the Night"},{"id":"B","option":"of a Shadow"},{"id":"C","option":"of the Puppets"},{"id":"D","option":"of his Mother"}],"value":500,"idAnswer":"A"},{"id":2,"question":"What is the way for a shadow to disappear?","options":[{"id":"A","option":"Taking a shower"},{"id":"B","option":"Running"},{"id":"C","option":"Turning off the light"}],"value":200,"idAnswer":"C"},{"id":3,"question":"Who dances and jumps with Tejus?","options":[{"id":"A","option":"his Mother"},{"id":"B","option":"Oruro"},{"id":"C","option":"his Shadow"}],"value":100,"idAnswer":"C"},{"id":4,"question":"¿Cuál es la capital de Bolivia?","options":[{"id":"A","option":"Tarija"},{"id":"B","option":"Oruro"},{"id":"C","option":"Potosí"},{"id":"D","option":"Sucre"}],"value":200,"idAnswer":"D"},{"id":5,"question":"¿Cuál es la capital de Bolivia?","options":[{"id":"A","option":"Tarija"},{"id":"B","option":"Oruro"},{"id":"C","option":"Potosí"},{"id":"D","option":"Sucre"}],"value":200,"idAnswer":"D"},{"id":6,"question":"¿Cuál es la capital de Bolivia?","options":[{"id":"A","option":"Tarija"},{"id":"B","option":"Oruro"},{"id":"C","option":"Potosí"},{"id":"D","option":"Sucre"}],"value":200,"idAnswer":"D"}]}
+  //var data = {"title":"Teju's Shadow","questions":[{"id":0,"question":"How do you describe a shadow?","options":[{"id":"A","option":"like a dark picture"},{"id":"B","option":"like a spy"},{"id":"C","option":"like a monster"}],"value":100,"idAnswer":"B"},{"id":1,"question":"¿What is the child in the story afraid?","options":[{"id":"A","option":"of the Night"},{"id":"B","option":"of a Shadow"},{"id":"C","option":"of the Puppets"},{"id":"D","option":"of his Mother"}],"value":500,"idAnswer":"A"},{"id":2,"question":"What is the way for a shadow to disappear?","options":[{"id":"A","option":"Taking a shower"},{"id":"B","option":"Running"},{"id":"C","option":"Turning off the light"}],"value":200,"idAnswer":"C"},{"id":3,"question":"Who dances and jumps with Tejus?","options":[{"id":"A","option":"his Mother"},{"id":"B","option":"his Schoolmates"},{"id":"C","option":"his Shadow"}],"value":100,"idAnswer":"C"},{"id":4,"question":"*** PENANCE ***","options":[{"id":"A","option":"Scream very loud"}],"value":100,"idAnswer":"A"},{"id":5,"question":"What did Teju's mother do when he tells her that is afraid of the dark?","options":[{"id":"A","option":"she gave him a big hug"},{"id":"B","option":"she gave him a stuffed animal"},{"id":"C","option":"she kissed him"}],"value":400,"idAnswer":"A"},{"id":6,"question":"*** PENANCE ***","options":[{"id":"A","option":"Tell someone you love him"}],"value":100,"idAnswer":"A"}]}
 
   var wheel = new Wheel("#Wheel1", 7, 10);
   var test = new Test("#Test1");
@@ -13,7 +13,11 @@ $(function () {
 
   //join event whell with the test
   wheel.endRotate = function (position) {
-    var question = test.getQuestion(position);
+    showQuestion(position);
+  }
+
+  function showQuestion(id){
+    question = test.getQuestion(id);
     dialog.render({ item: { question }, selector: ".question" });
     dialog.render({ item: question.options, selector: ".options", objName: "option" });
     dialog.showModal();
